9fd2d4a84b2c0e53f4714a29be3ce9b9c5f6fa9f
NguyenPhuThanh29082008 <nguyenphuthanhpch298@gmail.com>
2024-11-18 09:05:04 +0700

    Dùng M_PI để thay thế gán pi

--- Bai12_ChuViDienTichHinhTron.cpp
+++ Bai12_ChuViDienTichHinhTron.cpp
@@ -1,21 +1,20 @@
 // Bai12_ChuViDienTichHinhTron.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#define _USE_MATH_DEFINES
 #include <iostream>
 using namespace std;
 int main()
 {
-    double bankinh, pi, chuvi, dientich;
+    double bankinh, chuvi, dientich;
 
+    cout << "Tinh Dien Tich Va Chu Vi Hinh Tron\n";
     cout << "Nhap bankinh: ";  cin >> bankinh;
-    cout << "Ta co pi = 3.14\n";
-    pi = 3.14;
 
     cout << "Chu vi hinh tron la: \n";
-    chuvi = 2 * pi * bankinh;
-    dientich = pi * (bankinh*bankinh);
-    cout << "ta co: " << "2*" << pi << "*" << bankinh << "=" << chuvi << endl;
-    cout << "ta co: " << pi << "*" << bankinh << "^2" << "=" << dientich << endl;
+    chuvi = 2 * M_PI * bankinh;
+    dientich = M_PI * (bankinh*bankinh);
+    cout << "ta co: " << "2*" << M_PI << "*" << bankinh << "=" << chuvi << endl;
+    cout << "ta co: " << M_PI << "*" << bankinh << "^2" << "=" << dientich << endl;
 
     system("pause");
     return 0;
